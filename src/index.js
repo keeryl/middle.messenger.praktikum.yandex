a561a326bba75272be7a2fd7077619ac5b81bb6f
Кирилл Зикевский <2309696@mail.ru>
2022-11-09 00:08:54 +0300

    dev: dev in progress

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import chatTemplate from './pages/chat/chat.hbs';
+import page_404Template from './pages/page_404/page_404.hbs';
+import page_500Template from './pages/page_500/page_500.hbs';
+import profileTemplate from './pages/profile/profile.hbs';
+import signinTemplate from './pages/signin/signin.hbs';
+import signupTemplate from './pages/signup/signup.hbs';
+
+
+const PAGES = {
+  'chat': chatTemplate,
+  'page_404': page_404Template,
+  'page_500': page_500Template,
+  'profie': profileTemplate,
+  'signin': signinTemplate,
+  'signup': signupTemplate,
+}
+
+function renderPage(name) {
+  const root = document.querySelector('#app');
+  const template = PAGES[name];
+  const html = template();
+  root.innerHTML = html;
+}
+
+window.renderPage = renderPage;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('#app');
+  const html = signinTemplate();
+  root.innerHTML = html;
+});
