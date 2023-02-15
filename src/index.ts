d829831b02db9deb22a55d36cf5e66912e54f700
Кирилл Зикевский <2309696@mail.ru>
2023-02-15 14:37:15 +0300

    dev: dev in progress

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,56 @@ import Profile from './pages/Profile/Profile';
 import Signin from './pages/Signin/Signin';
 import Signup from './pages/Signup/Signup';
 import useInputValidation from '../src/utils/inputValidator';
+import Router from './utils/Router';
 
-const [formValues, errors, validateInput, validateForm] = useInputValidation();
+// const [formValues, errors, validateInput, validateForm] = useInputValidation();
 
-type Pages = {
-  [key: string]: typeof Block
+// type Pages = {
+//   [key: string]: typeof Block
+// }
+
+enum Routes {
+  'Home' = '/',
+  'Profile' = '/profile',
+  'Chat' = '/chat',
+  'Signin' = '/signin',
+  'Signup' = '/signup',
+  'Page_404' = '/page_404',
+  'Page_500' = '/page_500'
 }
 
-const PAGES: Pages = {
-  'chat': Chat,
-  'page_404': Page_404,
-  'page_500': Page_500,
-  'profile': Profile,
-  'signin': Signin,
-  'signup': Signup,
-}
+// const PAGES: Pages = {
+//   'chat': Chat,
+//   'page_404': Page_404,
+//   'page_500': Page_500,
+//   'profile': Profile,
+//   'signin': Signin,
+//   'signup': Signup,
+// }
 
-function renderPage(name: string): void {
-  const root: HTMLElement | null = document.querySelector('#app');
-  const component = PAGES[name];
-  const page = new component({ formValues, errors, validateInput, validateForm });
-  root!.innerHTML = '';
-  root!.append(page.getContent());
-}
-
-(window as any).renderPage = renderPage;
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.querySelector('#app');
-  const chat = new Chat({formValues, errors, validateInput, validateForm});
-  root!.append(chat.getContent());
+window.addEventListener('DomContentLoaded', async () => {
+  Router
+    .use(Routes.Home, Profile)
+    .use(Routes.Chat, Chat)
+    .use(Routes.Profile, Profile)
+    .use(Routes.Signin, Signin)
+    .use(Routes.Signup, Signup)
+    .use(Routes.Page_404, Page_404)
+    .use(Routes.Page_500, Page_500)
+    .start();
 });
+
+// function renderPage(name: string): void {
+//   const root: HTMLElement | null = document.querySelector('#app');
+//   const component = PAGES[name];
+//   const page = new component({ formValues, errors, validateInput, validateForm });
+//   root!.innerHTML = '';
+//   root!.append(page.getContent());
+// }
+
+// (window as any).renderPage = renderPage;
+// document.addEventListener('DOMContentLoaded', () => {
+//   const root = document.querySelector('#app');
+//   const chat = new Chat({formValues, errors, validateInput, validateForm});
+//   root!.append(chat.getContent());
+// });
--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,33 +1,76 @@
-export class EventBus<E extends { [Ev: string]: unknown[] }> {
-  private readonly listeners: {
-    [K in keyof E]?: Array<(...args: E[K]) => void>
-  };
-  constructor() {
-    this.listeners = {}
-  };
+// export class EventBus<E extends { [Ev: string]: unknown[] }> {
+//   private readonly listeners: {
+//     [K in keyof E]?: Array<(...args: E[K]) => void>
+//   };
+//   constructor() {
+//     this.listeners = {}
+//   };
 
-  on<K extends keyof E>(event: K, callback: (...args: E[K]) => void ): void {
+//   on<K extends keyof E>(event: K, callback: (...args: E[K]) => void ): void {
+//     if (!this.listeners[event]) {
+//       this.listeners[event] = [];
+//     }
+//     this.listeners[event]!.push(callback);
+//   }
+
+//   off<K extends keyof E>(event: K, callback: (...args: E[K]) => void):  void | never {
+//     if (!this.listeners[event]) {
+//       throw new Error(`Нет события ${event as string}`)
+//     }
+//     this.listeners[event] = this.listeners[event]!.filter(
+//       listener => listener !== callback
+//     );
+//   }
+
+//   emit<K extends keyof E>(event: K, ...args: E[K]): void | never {
+//     if (!this.listeners[event]) {
+//       throw new Error(`Нет события ${event as string}`)
+//     }
+//     this.listeners[event]!.forEach(listener => {
+//       listener(...args);
+//     })
+//   }
+// }
+
+
+
+type Handler<A extends any[] = unknown[]> = (...args: A) => void;
+type MapInterface<P> = P[keyof P]
+
+export class EventBus<
+  E extends Record<string, string> = Record<string, string>,
+  Args extends Record<MapInterface<E>, any[]> = Record<string, any[]>
+> {
+  private readonly listeners: {
+    [K in MapInterface<E>]?: Handler<Args[K]>[]
+  } = {};
+
+  on<Event extends MapInterface<E>>(event: Event, callback: Handler<Args[Event]>) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
-    this.listeners[event]!.push(callback);
+
+
+    this.listeners[event]?.push(callback);
   }
 
-  off<K extends keyof E>(event: K, callback: (...args: E[K]) => void):  void | never {
+  off<Event extends MapInterface<E>>(event: Event, callback: Handler<Args[Event]>) {
     if (!this.listeners[event]) {
-      throw new Error(`Нет события ${event as string}`)
+      throw new Error(`Нет события: ${event}`);
     }
+
     this.listeners[event] = this.listeners[event]!.filter(
       listener => listener !== callback
     );
   }
 
-  emit<K extends keyof E>(event: K, ...args: E[K]): void | never {
+  emit<Event extends MapInterface<E>>(event: Event, ...args: Args[Event]) {
     if (!this.listeners[event]) {
-      throw new Error(`Нет события ${event as string}`)
+      return;
     }
+
     this.listeners[event]!.forEach(listener => {
       listener(...args);
-    })
+    });
   }
 }
