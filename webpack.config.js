a49da8e0ac61075c1f96dab4abcfe95c9dd36959
Кирилл Зикевский <2309696@mail.ru>
2023-03-26 13:29:31 +0300

    dev: dev in progress

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'keeryl_messenger.bundle.js'
+  },
+  watch: true,
+  devServer: {
+    //contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
+    hot: true,
+    https: true,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        },
+        {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+        },
+    ]
+  }
+};
+
+
+
