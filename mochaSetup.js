7f370f77a55936928bece7ac0417ea795ab314f9
Кирилл Зикевский <2309696@mail.ru>
2023-04-06 02:03:23 +0300

    dev: dev in progress

---
+++ mochaSetup.js
@@ -0,0 +1,15 @@
+const { JSDOM } = require('jsdom');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+
+const { window } = new JSDOM('<div id="app"></div>', {
+  url: 'http://localhost:3000'
+});
+
+global.window = window;
+global.document = window.document;
+global.DocumentFragment = window.DocumentFragment;
+
+require.extensions['.css'] = function () {
+  module.exports = {}
+}
