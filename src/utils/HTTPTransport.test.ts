2b1e5d07f17ead623c719a2078695d0c2baf70a4
Кирилл Зикевский <2309696@mail.ru>
2023-04-07 23:38:21 +0300

    dev: dev in progress

---
+++ src/utils/HTTPTransport.test.ts
@@ -0,0 +1,83 @@
+import sinon from 'sinon';
+import HTTPTransport from './HTTPTransport';
+import { expect } from 'chai';
+
+describe('HTTPTransport class', () => {
+
+  let requests: sinon.SinonFakeXMLHttpRequest[] = [];
+  const originalXHR = global.XMLHttpRequest
+
+  before(() => {
+    const XHR = sinon.useFakeXMLHttpRequest()
+    //@ts-ignore
+    global.XMLHttpRequest = XHR;
+
+    XHR.onCreate = (xhr) => {
+      requests.push(xhr);
+    }
+  });
+
+  afterEach(() => {
+    requests = []
+  });
+
+  after(() => {
+    global.XMLHttpRequest = originalXHR;
+  });
+
+  describe('GET method', () => {
+
+    it('should make GET request', () => {
+      const transport = new HTTPTransport('/');
+      transport.get();
+      const request = requests[0];
+      expect(request.method).to.eq('Get');
+    });
+
+  });
+
+  describe('POST method', () => {
+
+    it('should make POST request', () => {
+      const transport = new HTTPTransport('/');
+      transport.post();
+      const request = requests[0];
+      expect(request.method).to.eq('Post');
+    });
+
+  });
+
+  describe('PUT method', () => {
+
+    it('should make PUT request', () => {
+      const transport = new HTTPTransport('/');
+      transport.put();
+      const request = requests[0];
+      expect(request.method).to.eq('Put');
+    });
+
+  });
+
+  describe('PATCH method', () => {
+
+    it('should make PATCH request', () => {
+      const transport = new HTTPTransport('/');
+      transport.patch();
+      const request = requests[0];
+      expect(request.method).to.eq('Patch');
+    });
+
+  });
+
+  describe('DELETE method', () => {
+
+    it('should make DELETE request', () => {
+      const transport = new HTTPTransport('/');
+      transport.delete();
+      const request = requests[0];
+      expect(request.method).to.eq('Delete');
+    });
+
+  });
+
+});
