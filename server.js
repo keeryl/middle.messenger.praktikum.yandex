1fca62fa47e133a7450b72ed2df2d5e66e089830
Кирилл Зикевский <2309696@mail.ru>
2023-04-08 17:35:21 +0300

    dev: dev in progress

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
